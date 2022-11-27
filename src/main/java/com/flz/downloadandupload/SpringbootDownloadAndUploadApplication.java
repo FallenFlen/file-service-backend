352a90876099e4ea24b81c045d8115611a8d287f
Fallenz <beijuspsn@163.com>
2022-11-27 14:52:17 +0800

    init project

---
+++ src/main/java/com/flz/downloadandupload/SpringbootDownloadAndUploadApplication.java
@@ -0,0 +1,13 @@
+package com.flz.downloadandupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDownloadAndUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDownloadAndUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flz/downloadandupload/SpringbootDownloadAndUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flz.downloadandupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDownloadAndUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
