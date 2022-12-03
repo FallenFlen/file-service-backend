0bad629c14612ddedb008d345fb4cc6ce6ab16a0
Fallenz <beijuspsn@163.com>
2022-12-03 18:09:52 +0800

    use @RestControllerAdvice

--- src/main/java/com/flz/downloadandupload/exception/handle/GlobalExceptionHandler.java
+++ src/main/java/com/flz/downloadandupload/exception/handle/GlobalExceptionHandler.java
@@ -5,11 +5,11 @@ import com.flz.downloadandupload.exception.BusinessException;
 import com.flz.downloadandupload.exception.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
